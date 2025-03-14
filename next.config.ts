752fae113cb9a90f8b98548668e1690b390eb492
Gayatri Patil <gayubhole2012@gmail.com>
2025-03-14 14:10:55 -0700

    design main part complete

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // You can still keep other experimental features if needed
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
