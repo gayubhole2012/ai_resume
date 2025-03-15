38f5a1cae43d7c1b8c4f9af228f4d69ea08f315d
Gayatri Patil <gayubhole2012@gmail.com>
2025-03-14 18:59:43 -0700

    main page is landed

--- app/page.js
+++ app/page.js
@@ -3,15 +3,20 @@ import Herosection from "@/components/hero";
 import { features } from "./data/features";
 import { howItWorks } from "./data/howItWorks";
 import { Card, CardContent } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import {
+  ArrowRight
+} from "lucide-react";
 import { testimonial } from "./data/testimonial";
 import { faqs } from "./data/faqs";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -157,6 +162,30 @@ export default function Home() {
               })}
             </Accordion>
 
+          </div>
+        </div>
+      </section>
+
+      {/* CTA Section */}
+      <section className="w-full">
+        <div className="mx-auto py-24 gradient rounded-lg w-full">
+          <div className="flex flex-col items-center justify-center space-y-4 text-center max-w-3xl mx-auto bg-gray-400">
+            <h2 className="text-3xl font-bold tracking-tighter text-primary-foreground sm:text-4xl md:text-5xl">
+              Ready to Accelerate Your Career?
+            </h2>
+            <p className="mx-auto max-w-[600px] text-primary-foreground/80 md:text-xl">
+              Join thousands of professionals who are advancing their careers
+              with AI-powered guidance.
+            </p>
+            <Link href="/dashboard" passHref>
+              <Button
+                size="lg"
+                variant="secondary"
+                className="h-11 mt-5 animate-bounce"
+              >
+                Start Your Journey Today <ArrowRight className="ml-2 h-4 w-4" />
+              </Button>
+            </Link>
           </div>
         </div>
       </section>
