4f19e280b8e7a3a525865eb06d6fb41049b3a0cd
Gayatri Patil <gayubhole2012@gmail.com>
2025-04-03 14:34:05 -0700

    Industry Insight whole page done

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,14 +1,18 @@
 import { getUserOnBoardingStatus } from "@/actions/user";
 import { redirect } from "next/navigation";
+import { getIndustryInsights } from "../../../actions/dashboard";
+import DashboardView from "./_components/dashboard-view";
 
 
 const IndustryInsightsPage = async () => {
     const { isOnboarded } = await getUserOnBoardingStatus();
+    const insights = await getIndustryInsights();
 
     if (!isOnboarded) {
         redirect("/onboarding");
     }
-    return <div>IndustryInsightsPage</div>
-};
-
+    return <div className="container mx-auto">
+        <DashboardView insights={insights} />
+    </div>
+}
 export default IndustryInsightsPage;
